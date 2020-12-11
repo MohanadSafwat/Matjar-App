8f81f8507629fa2c1d77ebd3e88ab2314bfcd9d6
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2020-12-12 01:06:53 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'matjar_icons1.dart';
+import 'profileInfo.dart';
+import 'matjar_icons.dart';
+import 'profileLoggedIn.dart';
+import 'profile.dart';
+void main() {
+  runApp(MaterialApp(home: ProfileLoggedIn()));
+}
+
