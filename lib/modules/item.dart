af0badd22b09195316f81e8d83b96b0c9bb804f1
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2020-12-15 03:25:20 +0200

    Create item.dart

---
+++ lib/modules/item.dart
@@ -0,0 +1,41 @@
+import './review.dart';
+
+class Item {
+  String id, categoryName, name, brand, sellerId;
+  int offer = 0, numberInStock, numberOfReviews;
+  double price, rate;
+  bool inStock;
+  Map<String, String> specs = {};
+  List<Review> reviews = [];
+  List<String> photosUrl = [];
+
+  Item(
+      {this.id,
+      this.brand,
+      this.categoryName,
+      this.inStock,
+      this.name,
+      this.numberInStock,
+      this.numberOfReviews,
+      this.offer,
+      this.photosUrl,
+      this.price,
+      this.rate,
+      this.reviews,
+      this.sellerId,
+      this.specs});
+
+  double newPrice() => price * (100 - offer) / 100;
+
+  double starClac() {
+    double sum = 0;
+    for (var n in reviews) {
+      sum += n.numberOfStars;
+    }
+    return sum / reviews.length;
+  }
+
+  bool isInStock() {
+    return numberInStock > 0;
+  }
+}
