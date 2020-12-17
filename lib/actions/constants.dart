83442f941640d98f5985ebe4ac17834922ad17d6
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2020-12-17 23:53:57 +0200

    Create constants.dart

---
+++ lib/actions/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+ const itemName ='itemName';
+ const itemPrice='itemPrice';
+ const itemBrand='itemBrand';
+ const noOfItemsInStock= 'noOfItemsInStock';
+ const itemSpecs='itemSpecs';
+ const itemPhotos='itemPhotos';
