83de709c13e78260bc329cfb94aec92516c2e997
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2020-12-17 23:53:48 +0200

    Create productActions.dart

---
+++ lib/actions/productActions.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:matjar_login_signup/modules/item.dart';
+import './constants.dart';
+class ProductAction{
+ // FirebaseFirestore firestore = FirebaseFirestore.instance.collection('mobiles');
+
+CollectionReference firestore = FirebaseFirestore.instance.collection('Category');
+
+ Future<void>addProduct(Item item,String category ){
+  return firestore.doc(category).collection('items').doc('2').set({
+    itemName : item.name,
+    itemPrice :item.price,
+    itemBrand:item.brand,
+    noOfItemsInStock:item.numberInStock,
+     });
+}
+}
