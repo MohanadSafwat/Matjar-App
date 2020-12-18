917d4a0dce8ffeef7385d1adf07c5ce8c8408991
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2020-12-18 09:15:33 +0200

    Update productActions.dart

--- lib/actions/productActions.dart
+++ lib/actions/productActions.dart
@@ -1,18 +1,97 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:matjar_login_signup/modules/item.dart';
+import 'package:permission_handler/permission_handler.dart';
 import './constants.dart';
+var photoUrl;
 class ProductAction{
- // FirebaseFirestore firestore = FirebaseFirestore.instance.collection('mobiles');
+  Item item;
 
-CollectionReference firestore = FirebaseFirestore.instance.collection('Category');
 
- Future<void>addProduct({Item item} ){
-  return firestore.doc(item.categoryName).collection('items').doc('2').set({
+
+  // FirebaseFirestore firestore = FirebaseFirestore.instance.collection('mobiles');
+  ProductAction(this.item);
+CollectionReference firestore = FirebaseFirestore.instance.collection('Categories');
+
+ Future<void>addProduct(String itemId){
+
+  return firestore.doc(item.categoryName).collection('items').doc(itemId).set({
     itemName : item.name,
     itemPrice :item.price,
     itemBrand:item.brand,
     noOfItemsInStock:item.numberInStock,
-     });
+    'photoUrl' : photoUrl,
+    itemSpecs: item.specs,
+     })
+      .then((value) => print("Product Added"))
+      .catchError((error) => print("Failed to add product: $error"));
+
 }
+
 }
+
+Future<String> uploadImage() async {
+
+  final _storage = FirebaseStorage.instance;
+  final _picker = ImagePicker();
+
+  //Check Permissions
+  await Permission.photos.request();
+
+  var permissionStatus = await Permission.photos.status;
+
+  if (permissionStatus.isGranted){
+    //Select Image
+    final image = await _picker.getImage(source: ImageSource.gallery);
+    final file = File(image.path);
+
+    if (image != null){
+      //Upload to Firebase
+      var snapshot = await _storage.ref()
+          .child('itemPhotos')
+          .putFile(file);
+
+
+      var downloadUrl = await snapshot.ref.getDownloadURL();
+      photoUrl = downloadUrl;
+
+    } else {
+      print('No Path Received');
+    }
+
+
+  } else {
+    print('Grant Permissions and try again');
+  }}
+int count;
+   counter()async{
+await    FirebaseFirestore.instance
+        .collection('variable')
+        .doc('itemIdCounter')
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+         count = documentSnapshot.data()['counter'];
+      } else {
+        print('Document does not exist on the database');
+      }
+    });
+  }
+  int getCounter(){
+  return count;
+  }
+  addCounter(int counterPlused)async{
+     await
+     counterPlused++;
+     FirebaseFirestore.instance
+         .collection('variable')
+         .doc('itemIdCounter')
+         .set({'counter':counterPlused});
+
+
+  }
