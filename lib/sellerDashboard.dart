bbd869d2f498e1ff4a258fbf2f31d24dac0ebff6
Mohaned Elhadidi <mohaned_hadidi@yahoo.com>
2020-12-19 15:39:37 +0200

    Testing pushing to Github

--- lib/sellerDashboard.dart
+++ lib/sellerDashboard.dart
@@ -8,7 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-
+//test
 var white = Colors.white;
 int currentIndex = 3;
 
