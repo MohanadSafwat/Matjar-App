c951b8e73626e808f6291a7ce23d51802ff0471a
Ahmed Alaa <a99_bdr@yahoo.com>
2020-12-22 18:23:11 +0200

    yaml and main edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:matjar_login_signup/Cart.dart';
-import 'package:matjar_login_signup/emptyCart.dart';
-import 'package:matjar_login_signup/items.dart';
-import 'package:matjar_login_signup/payment.dart';
-import 'package:matjar_login_signup/cartItem.dart';
-import 'package:matjar_login_signup/selected_item.dart';
+import 'Cart.dart';
+import 'emptyCart.dart';
+import 'items.dart';
+import 'payment.dart';
+import 'cartItem.dart';
+import 'selected_item.dart';
 import 'package:provider/provider.dart';
 import 'login.dart';
 import 'signUp.dart';
@@ -15,7 +15,12 @@ import 'profileInfo.dart';
 import 'matjar_icons.dart';
 import 'profileLoggedIn.dart';
 import 'profile.dart';
-import 'package:matjar_login_signup/emptyCart.dart';
+import 'ProfileEdit.dart';
+import 'SignUpSeller.dart';
+import 'profileEditSeller.dart';
+import 'profileInfoSeller.dart';
+import 'ProfileLoginSeller.dart';
+import 'loginSeller.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -29,23 +34,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-ChangeNotifierProvider<cartItem>(
-  create: (context) => cartItem(),)
- ],
-    
-    child: MaterialApp(
-      //title: "Navigation",
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        Cart.id: (context) => Cart(),
-        SelectedItem.id: (context)=> SelectedItem(),
-        Payment.id: (context)=> Payment(),
-        '/': (context) => LandingPage(),
-        '/Login': (context) => Login(),
-        '/SignUp': (context) => SignUp(),
-        '/Home': (context) => MyHomePage(),
-      },
-    ),
+        ChangeNotifierProvider<cartItem>(
+          create: (context) => cartItem(),
+        )
+      ],
+      child: MaterialApp(
+        //title: "Navigation",
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          Cart.id: (context) => Cart(),
+          SelectedItem.id: (context) => SelectedItem(),
+          Payment.id: (context) => Payment(),
+          '/': (context) => LandingPage(),
+          '/Login': (context) => Login(),
+          '/SignUp': (context) => SignUp(),
+          '/Home': (context) => MyHomePage(),
+          '/Profile': (context) => Profile(),
+          '/ProfileInfo': (context) => ProfileInfo(),
+          '/ProfileEdit': (context) => ProfileEdit(),
+          '/ProfileEditSeller': (context) => ProfileEditSeller(),
+          '/ProfileLoginSeller': (context) => ProfileLogInSeller(),
+          '/ProfileLoggedIn': (context) => ProfileLoggedIn(),
+          '/SignUpSeller': (context) => SignUpSeller(),
+          '/ProfileInfoSeller': (context) => ProfileInfoSeller(),
+          '/LoginSeller': (context) => LoginSeller(),
+        },
+      ),
     );
   }
 }
