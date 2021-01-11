b0afd82007a0a835bf279a98184870e01cd57018
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2021-01-11 18:33:35 +0200

    Update productActions.dart

--- lib/actions/productActions.dart
+++ lib/actions/productActions.dart
@@ -7,6 +7,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:matjar_login_signup/modules/item.dart';
+import 'package:matjar_login_signup/modules/user.dart';
 import 'package:permission_handler/permission_handler.dart';
 import './constants.dart';
 
@@ -17,11 +18,12 @@ class ProductAction {
 
   // FirebaseFirestore firestore = FirebaseFirestore.instance.collection('mobiles');
   // ProductAction(this.item);
-  CollectionReference firestore =
+  CollectionReference firestoreCategories =
       FirebaseFirestore.instance.collection('Categories');
-  Future<void> addProduct(Item item, String itemId) {
+  Future<void> addProductToCategories(Item item, String itemId) {
+    addProductToProducts(item, itemId);
     addProductIdToSeller(itemId, item.categoryName, item.sellerId);
-    return firestore
+    return firestoreCategories
         .doc(item.categoryName)
         .collection('items')
         .doc(itemId)
@@ -33,11 +35,50 @@ class ProductAction {
           itemPhotoUrl: photoUrl,
           itemSpecs: item.specs,
           itemSellerId: item.sellerId,
+          'itemCategoryName': item.categoryName,
+          'rate': 0
         })
-        .then((value) => print("Product Added"))
+        .then((value) => print("Product Added to category"))
+        .catchError((error) => print("Failed to add product: $error"));
+  }
+
+  CollectionReference firestoreProducts =
+      FirebaseFirestore.instance.collection('products');
+  Future<void> addProductToProducts(Item item, String itemId) {
+    return firestoreProducts
+        .doc(itemId)
+        .set({
+          itemName: item.name,
+          itemPrice: item.price,
+          itemBrand: item.brand,
+          noOfItemsInStock: item.numberInStock,
+          itemPhotoUrl: photoUrl,
+          itemSpecs: item.specs,
+          itemSellerId: item.sellerId,
+          'itemCategoryName': item.categoryName,
+          'rate': 0,
+          'itemReviews': FieldValue.arrayUnion([]),
+
+    })
+        .then((value) => print("Product Added to products"))
         .catchError((error) => print("Failed to add product: $error"));
   }
 }
+var cartItems =<Map>[];
+Future<void> addProductToCart(Item item, String userId) {
+  CollectionReference user =FirebaseFirestore.instance.collection('Users');
+  var map = {};
+  map = {'itemCategoryName': item.categoryName, 'itemId': item.id, 'itemPrice':item.price,
+    'itemUrl':item.url, 'itemName':item.name
+  };
+  cartItems.add(map);
+  return user.doc(userId).update({
+    'cartItems': FieldValue.arrayUnion(cartItems),
+    // :FieldValue.arrayUnion(itemCategoryName),
+    //
+  }).then((value) => print('add product to cart'));
+}
+
 
 // List<Map<String,String>>SellerItems=[];
 var sellerItem = <Map>[];
@@ -48,11 +89,11 @@ Future<void> addProductIdToSeller(
   var map = {};
   map = {'itemCategoryName': itemCategoryName, 'itemId': itemId};
   sellerItem.add(map);
-  return firestore2.doc(sellerId).set({
+  return firestore2.doc('3Nd2P2oDZDUdOuLqLYYewwH6jDs2').update({
     'items': FieldValue.arrayUnion(sellerItem),
     // :FieldValue.arrayUnion(itemCategoryName),
     //
-  });
+  }).then((value) => print('add to seller'));
 }
 
 Future<String> uploadImage() async {
@@ -81,6 +122,10 @@ Future<String> uploadImage() async {
   } else {
     print('Grant Permissions and try again');
   }
+}
+
+getUrl() {
+  return photoUrl;
 }
 
 int count;
@@ -118,6 +163,13 @@ getSellerId() async {
   userID = uid;
 }
 
+getUserId() {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User user = auth.currentUser;
+  final uid = user.uid;
+  userID = uid;
+}
+
 String sendID() {
   return userID;
 }
@@ -127,29 +179,92 @@ Future getSellerProductId(String id) async {
   try {
     await FirebaseFirestore.instance
         .collection("sellers")
-        .doc(id)
+        .doc('3Nd2P2oDZDUdOuLqLYYewwH6jDs2')
         .get()
         .then((documentsnapshot) {
       productId = documentsnapshot.data()["items"];
     });
-  
   } catch (e) {
     print(e.toString());
     return null;
   }
 }
 
+Stream<DocumentSnapshot> Product(String id) {
+  CollectionReference firebase =
+      FirebaseFirestore.instance.collection('products');
+  return firebase.doc(id).snapshots();
+}
+
 Future getSellerProducts() async {
   List products = [];
   getSellerId();
-  getSellerProductId(sendID());
+  await getSellerProductId('3Nd2P2oDZDUdOuLqLYYewwH6jDs2');
   CollectionReference firestore2 =
       FirebaseFirestore.instance.collection("products");
-for (var item in productId) {
+
+  for (var item in productId) {
     await firestore2.doc(item["itemId"]).get().then((documentsnapshot) {
       products.add(documentsnapshot.data());
     });
-    return products;
   }
+  return products;
+}
 
+addReviewToProducts(
+    Map<String, dynamic> info, String itemId, String comment, double rate) {
+  CollectionReference firestore2 =
+      FirebaseFirestore.instance.collection("products");
+  var map = {};
+  map = {
+    'firstName': info['firstName'],
+    'lastName': info['lastName'],
+    'comment': comment,
+    'noOfStars': rate
+  };
+  sellerItem.add(map);
+  return firestore2.doc(itemId).update({
+    'itemReviews': FieldValue.arrayUnion(sellerItem),
+    // :FieldValue.arrayUnion(itemCategoryName),
+    //
+  }).then((value) => print('add review to products'));
+}
+
+addReviewToCategories(Map<String, dynamic> info, String itemId, String comment,
+    double rate, String categoryName) {
+  addReviewToProducts(info, itemId, comment, rate);
+  CollectionReference firestore3 = FirebaseFirestore.instance
+      .collection("Categories")
+      .doc(categoryName)
+      .collection('items');
+
+  return firestore3.doc(itemId).update({
+    'itemReviews': FieldValue.arrayUnion(sellerItem),
+    // :FieldValue.arrayUnion(itemCategoryName),
+    //
+  }).then((value) => print('add review to category'));
+}
+
+upateRateOnProducts(double rate, String itemId) {
+  CollectionReference firestore4 =
+      FirebaseFirestore.instance.collection("products");
+  print('done 1');
+
+  return firestore4.doc(itemId).update({
+    'rate': rate,
+    // :FieldValue.arrayUnion(itemCategoryName),
+    //
+  }).then((value) => print('update rate to products'));
+}
+
+upateRateOnCategories(double rate, String itemId, String categoryName) {
+  upateRateOnProducts(rate, itemId);
+  print('done 2');
+  CollectionReference firestore3 = FirebaseFirestore.instance
+      .collection("Categories")
+      .doc(categoryName)
+      .collection('items');
+  return firestore3.doc(itemId).update({
+    'rate': rate,
+  }).then((value) => print('update rate to category'));
 }
