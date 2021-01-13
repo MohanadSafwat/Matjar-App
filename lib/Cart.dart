d8d62e72a52e17c82f01b57611584fbde57afc51
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-01-13 15:30:01 +0200

    Update Cart.dart

--- lib/Cart.dart
+++ lib/Cart.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:matjar_login_signup/constants.dart';
 import 'package:matjar_login_signup/payment.dart';
@@ -9,7 +10,9 @@ import 'package:matjar_login_signup/cartItem.dart';
 import 'package:matjar_login_signup/modules/item.dart';
 import 'package:provider/provider.dart';
 import 'package:matjar_login_signup/custom_menu.dart';
+import 'package:matjar_login_signup/actions/productActions.dart';
 
+import 'modules/user.dart';
 
 class Cart extends StatelessWidget {
   static String id = 'Cart';
@@ -17,13 +20,13 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-List<Item> items= Provider.of<cartItem>(context).items;
-final double screenHeight=MediaQuery.of(context).size.height;
-final double screenwidth=MediaQuery.of(context).size.width;
+    final user = Provider.of<Userinit>(context);
+    List<Item> items = Provider.of<cartItem>(context).items;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenwidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           "Matjar",
@@ -36,7 +39,7 @@ backgroundColor: Colors.white,
         toolbarHeight: 80,
         centerTitle: true,
       ),
-bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: 30,
         currentIndex: currentIndex,
@@ -80,250 +83,162 @@ bottomNavigationBar: BottomNavigationBar(
             });
           }, */
       ),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: CartItems(user.uid),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+          return LayoutBuilder(builder: (context, constrains) {
+            if (snapshot.data.data()['cartItems'] != null) {
+              return Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: GestureDetector(
+                            onTapUp: (details) {
+                              showCustomMenu(details, context, items[index]);
+                            },
+                            child: Container(
+                              height: screenHeight * 0.15,
+                              child: Row(
+                                children: <Widget>[
+                                  SizedBox(width:20 ,),
 
+                                  Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Image.network(snapshot.data.data()['cartItems'][index]['itemUrl']),
+                                        ),
+                                  SizedBox(width:20 ,),
 
-
-
- body:LayoutBuilder(
-  builder:(context,constrains){
-    if(items.isNotEmpty)
-    {
-return Column(
-children : <Widget>[
-
- Expanded(
-child:ListView.builder(itemBuilder:
-(context,index)
-{
-
-return Padding(
-  padding: const EdgeInsets.all(15),
-child: GestureDetector(
-onTapUp: (details){
-showCustomMenu(details,context,items[index]);
-
-},
-
-  child:   Container(
-  
-  
-  
-  height: screenHeight*0.15,
-  
-  child: Row(children:<Widget> [
-  
-  CircleAvatar(
-  
-  radius: screenHeight*0.15/2,
-  
-  backgroundImage: AssetImage(items[index].photosUrl[index]),
-  
-  
-  
-  ),
-  
-  Expanded(
-  
-  
-  
-  child:Row(
-  
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  
-  children: <Widget>[
-  
-  Padding(padding: const EdgeInsets.all(3),
-  
-  child:Column(
-  
-  mainAxisAlignment: MainAxisAlignment.center,
-  
-  children: <Widget>[
-  
-  
-  
-  Text (items[index].name
-  
-   ,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-  
-  ),
-  
-  
-  
-  Text (items[index].categoryName
-  
-   ,style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),
-  
-  ),
-  
-  Padding(padding: const EdgeInsets.all(7),
-  
-  child: Text ( '\$ ${items[index].price}'
-  
-   ,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-  
-  )
-  
-  ),
-  
-  ],
-  
-  
-  
-  )
-  
-  ),
-  
-  Padding(padding:const EdgeInsets.only(right:15),
-  
-  child:Text (items[index].itemQuantity.toString(),
-  
-  style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
-  
-  )
-  
-  )
-  
-  ],
-  
-  
-  
-  
-  
-  
-  
-  )
-  
-  )
-  
-  ],
-  
-  
-  
-  
-  
-  ),
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  color: Colors.white,
-  
-  ),
-),
-
-);
-},
-
-itemCount: items.length,
-),
-),
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ButtonTheme(
-  minWidth: screenwidth,
-child:RaisedButton(
-  shape: RoundedRectangleBorder(
-
-borderRadius: BorderRadius.only(
-topRight: Radius.circular(10),
-topLeft: Radius.circular(10),
-)
-),
-  
-  onPressed: ()
-{
-  Navigator.push(
-          context,
-         MaterialPageRoute(builder: (context) => Payment()));
-
-
-},
-child: Text('Order Now '.toUpperCase(),style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-
-color:Colors.red,
-
-),
-)
-
-],
-
-);
-  }
-  
-  else{
-return  Center(
-child: Text('Your Cart is Empty',style: TextStyle(fontSize: 30,color:Colors.red,fontWeight: FontWeight.bold)),
-
-);
-}
-    }
-    ),
-
+                                  Expanded(
+                                      child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Padding(
+                                          padding: const EdgeInsets.all(3),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text(
+                                                snapshot.data.data()['cartItems'][index]['itemName'],
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold),
+                                              ),
+                                              Text(
+                                                snapshot.data.data()['cartItems'][index]['itemCategoryName'],
+                                                style: TextStyle(
+                                                    fontSize: 13,
+                                                    fontWeight: FontWeight.bold),
+                                              ),
+                                              Padding(
+                                                  padding: const EdgeInsets.all(7),
+                                                  child: Text(
+                                                    'Egp ${snapshot.data.data()['cartItems'][index]['itemPrice']}',
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  )),
+                                            ],
+                                          )),
+                                      // Padding(
+                                      //     padding: const EdgeInsets.only(right: 15),
+                                      //     child: Text(
+                                      //       items[index].itemQuantity.toString(),
+                                      //       style: TextStyle(
+                                      //           fontSize: 15,
+                                      //           fontWeight: FontWeight.bold),
+                                      //     ))
+                                    ],
+                                  ))
+                                ],
+                              ),
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: snapshot.data.data()['cartItems'].length,
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: screenwidth,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(10),
+                      )),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Payment()));
+                      },
+                      child: Text(
+                        'Order Now '.toUpperCase(),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('Your Cart is Empty',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold)),
+              );
+            }
+          });}
+          else{
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: mainColor,
+                ),
+              ),
+            );
+          }
+        }
+      ),
     );
-    
   }
 
-  void showCustomMenu(details,context,item) async {
-double dx = details.globalPosition.dx;
-double dy = details.globalPosition.dy;
-double dx2= MediaQuery.of(context).size.width-dx;
-double dy2= MediaQuery.of(context).size.width-dy;
+  void showCustomMenu(details, context, item) async {
+    double dx = details.globalPosition.dx;
+    double dy = details.globalPosition.dy;
+    double dx2 = MediaQuery.of(context).size.width - dx;
+    double dy2 = MediaQuery.of(context).size.width - dy;
 
-await showMenu(
-
-context: context,
-position: RelativeRect.fromLTRB(dx, dy, dx2, dy2),
-items: [
-MyPopupMenuItem(
-onClick:(){
-Navigator.pop(context);
-Provider.of<cartItem>(context,listen: false).deleteItem(item);
-Navigator.pushNamed(context, SelectedItem.id,arguments: item);
-},
-child: Text('Edit'),
-),
-
-MyPopupMenuItem(
-onClick:(){
-Navigator.pop(context);
-Provider.of<cartItem>(context,listen: false).deleteItem(item);
-},
-
-child: Text('Delete'),
-),
-]
-);}
-
-
-
-
-
-
-
+    await showMenu(
+        context: context,
+        position: RelativeRect.fromLTRB(dx, dy, dx2, dy2),
+        items: [
+          MyPopupMenuItem(
+            onClick: () {
+              Navigator.pop(context);
+              Provider.of<cartItem>(context, listen: false).deleteItem(item);
+              Navigator.pushNamed(context, SelectedItem.id, arguments: item);
+            },
+            child: Text('Edit'),
+          ),
+          MyPopupMenuItem(
+            onClick: () {
+              Navigator.pop(context);
+              Provider.of<cartItem>(context, listen: false).deleteItem(item);
+            },
+            child: Text('Delete'),
+          ),
+        ]);
+  }
 }
